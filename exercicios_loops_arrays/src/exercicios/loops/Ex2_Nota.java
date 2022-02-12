5c458c748d52fe8af44b1cea197f80298172ad2b
adaatii <80161447+adaatii@users.noreply.github.com>
2022-02-12 14:48:21 -0300

    Estruturas de Repetição e Arrays em Java
    Exercicio 3

--- exercicios_loops_arrays/src/exercicios/loops/Ex2_Nota.java
+++ exercicios_loops_arrays/src/exercicios/loops/Ex2_Nota.java
@@ -2,6 +2,12 @@ package exercicios.loops;
 
 import java.util.Scanner;
 
+/*
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+*/
 
 public class Ex2_Nota {
     public static void main(String[] args) {
